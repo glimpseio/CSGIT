a18e6f5bf167d5074f6000ed13c5d1cf2527479d
marcprux <marc@glimpse.io>
2021-03-30 14:26:36 -0400

    ci: workflow_dispatch

--- verify.sh
+++ verify.sh
@@ -1,18 +1,20 @@
-#!/bin/sh
+#!/bin/sh -e
 # Correctness Verification
 # DO NOT EDIT – you probably want to be editing assignment.sh instead
 
-echo "Verifying Correctness in ${PWD}"
+cd "${1:-.}"
+
+echo "Verifying Correctness for ${PWD}"
 
 RESULT=`./assignment.sh 1 2`
 if [ "${RESULT}" != "3" ]; then 
-    echo "Bad T1 Result: ${RESULT}"
+    echo "Bad result: ${RESULT}"
     exit 1
 fi
 
 RESULT=`./assignment.sh 1 2 4`
 if [ "${RESULT}" != "7" ]; then 
-    echo "Bad Result: ${RESULT}"
+    echo "Bad result: ${RESULT}"
     exit 1
 fi
 
